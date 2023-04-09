ad93a5539f7c5183744d7fa71150971ac7fa7b3f
II0H4uk <126766004+II0H4uk@users.noreply.github.com>
2023-04-10 01:23:45 +0300

    Add Config

---
+++ Life/Config.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cli_life
+{
+    class Config
+    {
+        public int width;
+        public int height;
+        public int cellSize;
+        public double liveDensity;
+
+        public Config(string JSON)
+        {
+            string[] a = JSON.Split(new char[] { '\r', '\n', ' ', '\\', '{', '}', '\"', ':', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            width = int.Parse(a[1]);
+            height = int.Parse(a[3]);
+            cellSize = int.Parse(a[5]);
+            a[7] = a[7].Replace(".", ",");
+            liveDensity = double.Parse(a[7]);
+        }
+    }
+}
