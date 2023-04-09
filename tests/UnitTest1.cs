2a9efc66b43ef642478b737937ab95b86b2a38e1
II0H4uk <126766004+II0H4uk@users.noreply.github.com>
2023-04-10 00:14:41 +0300

    Add tests

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,90 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using cli_life;
+using System.IO;
+using System.Collections.Generic;
+
+namespace NET
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program test = new Program();
+            Program.Reset();
+            Program.Load("system1.txt");
+            Program.Render();
+            Assert.IsTrue(Program.countLive == 47);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Program test = new Program();
+            Program.Reset();
+            Program.Load("system on 72 step.txt");
+            Program.Render();
+            Assert.IsTrue(Program.countLive == 118);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Program test = new Program();
+            Program.Reset();
+            Program.Load("system1.txt");
+            Program.Render();
+            Program.Info();
+
+            string[] expected = File.ReadAllLines("expected1.txt");
+            List<string> expectedList = new List<string>();
+            foreach (string a in expected)
+                expectedList.Add(a);
+            for (int i = 0; i < expectedList.Count; i++)
+            {
+                Assert.AreEqual(expectedList[i], Program.check[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Program test = new Program();
+            Program.Reset();
+            Program.Load("system2.txt");
+            Program.Render();
+            Program.Info();
+
+            string[] expected = File.ReadAllLines("expected2.txt");
+            List<string> expectedList = new List<string>();
+            foreach (string a in expected)
+                expectedList.Add(a);
+            for (int i = 0; i < expectedList.Count; i++)
+            {
+                Assert.AreEqual(expectedList[i], Program.check[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Program test = new Program();
+            Program.Reset();
+            Program.Load("system3.txt");
+            Program.Render();
+            Program.Info();
+
+            string[] expected = File.ReadAllLines("expected3.txt");
+            List<string> expectedList = new List<string>();
+            foreach (string a in expected)
+                expectedList.Add(a);
+            for (int i = 0; i < expectedList.Count; i++)
+            {
+                Assert.AreEqual(expectedList[i], Program.check[i]);
+            }
+        }
+
+    }
+}
